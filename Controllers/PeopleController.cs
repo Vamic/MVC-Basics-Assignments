dd9e5b126de893de01d80391f485e09f8c892ab6
Carl König <made.swede@live.com>
2018-04-17 09:59:23 +0200

    People List

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Person.PeopleList == null)
+                Person.GenerateList();
+
+            return View(new Person());
+        }
+
+        public ActionResult Add(string name, string phone, string city)
+        {
+            if (Person.PeopleList != null)
+            {
+                if(name.Length < 2 || city.Length < 2) return new HttpStatusCodeResult(400);
+                Person.PeopleList.Add(new Person(name, phone, city));
+                return View("Index", new Person());
+            }
+            return new HttpStatusCodeResult(401);
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            if (Person.PeopleList != null)
+            {
+                Person found = Person.PeopleList.FirstOrDefault(item => item.ID == ID);
+                if (found != null)
+                {
+                    Person.PeopleList.Remove(found);
+                }
+                return View("Index", new Person());
+            }
+            return new HttpStatusCodeResult(401);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class Person
+    {
+        private static int PeopleCount = 0;
+        public int ID { get; private set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+
+        public static List<Person> PeopleList { get; set; }
+
+
+        public Person()
+        {
+            this.ID = PeopleCount;
+            PeopleCount++;
+        }
+
+        public Person(string name, string phone, string city) : this()
+        {
+            this.Name = name;
+            this.Phone = phone;
+            this.City = city;
+        }
+
+        public static void GenerateList()
+        {
+            PeopleList = new List<Person>
+            {
+                new Person("Thomas Whitty", "070-4324031", "GRÖNAHÖG"),
+                new Person("Orlando Bryce", "0981-6841735", "TOMTEBODA"),
+                new Person("Shawanna To", "0693-2595150", "BALLINGSLÖV"),
+                new Person("Broderick Oglesby", "0739-9681594", "ÖJE"),
+                new Person("Junko Schoolcraft", "0528-2784905", "SÖLVESBORG"),
+                new Person("Kellie Testerman", "08-2223857", "HARMÅNGER"),
+                new Person("Laurette Ottman", "0651-3015369", "KUNGSHAMN"),
+                new Person("Jerrica Milot", "033-2400723", "ÖDESBORG"),
+                new Person("Fermin Kazmierczak", "0923-8648243", "ÖDESBORG"),
+                new Person("Rayna Brown", "0494-8726761", "HISHULT")
+            };
+        }
+
+        public static List<Person> GetFilteredList(string search, bool caseSensitive)
+        {
+            if(caseSensitive)
+                return PeopleList.Where(person => person.Name.Contains(search) || person.City.Contains(search)).ToList();
+            search = search.ToLower();
+            return PeopleList.Where(person => person.Name.ToLower().Contains(search) || person.City.ToLower().Contains(search)).ToList();
+        }
+    }
+}
